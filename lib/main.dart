201db15922376e974bea6576efbadf4e96ecbc94
Harsh_master <harshsinghmarwah@gmail.com>
2020-03-23 13:42:35 +0530

    Initialized

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import './dialog1.dart';
+import './dialog2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Dialog Box -1'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: double.infinity,
+                      ),
+                      child: AlertDialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        contentPadding: EdgeInsets.all(0.0),
+                        content: SingleChildScrollView(
+                          child: Dialog1(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+            RaisedButton(
+              child: Text('Dialog Box -2'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SingleChildScrollView(
+                      child: AlertDialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        contentPadding: EdgeInsets.all(0.0),
+                        content: SingleChildScrollView(
+                          child: Dialog2(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+            // Dialog1(),
+          ],
+        ),
+      ),
+    );
+  }
+}
